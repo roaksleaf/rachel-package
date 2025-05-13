aafa3cc3aa40f2806e8cf43b4c17aeab9ae481f3
Vyom Raval <vyomr@uw.edu>
2025-05-13 11:00:43 -0700

    Init doves perturbation

---
+++ +edu/+washington/+riekelab/+rachel/+protocols/DovesPerturbation.m
@@ -0,0 +1,217 @@
+classdef DovesPerturbation < manookinlab.protocols.ManookinLabStageProtocol
+    
+    properties
+        preTime = 500 % ms
+        stimTime = 20000 % ms
+        tailTime = 500 % ms
+        stixelSize = 60 % um
+        stimulusIndices = [2]         % Stimulus number (1:161)
+        binaryNoise = true %binary checkers - overrides noiseStdv
+        pairedBars = true
+        noiseStdv = 0.3 %contrast, as fraction of mean
+        frameDwell = 1 % Frames per noise update
+        apertureDiameter = 0 % um
+        backgroundIntensity = 0.5 % (0-1)
+        onlineAnalysis = 'none'
+        numberOfAverages = uint16(60) % number of epochs to queue
+        amp % Output amplifier
+    end
+
+    properties (Hidden)
+        ampType
+        onlineAnalysisType = symphonyui.core.PropertyType('char', 'row', {'none', 'extracellular', 'exc', 'inh'})
+        projectionTypeType = symphonyui.core.PropertyType('char', 'row', {'none', 'linear filter'})
+        noiseSeed
+        noiseStream
+        numChecksX
+        numChecksY
+        imageMatrix
+        lineMatrix
+        dimBackground
+        useFixedSeed = true     % Toggle between fixed and random seeds
+        im
+        pkgDir
+        currentStimSet
+        stimulusIndex
+        freezeFEMs = true
+        xTraj
+        yTraj
+        timeTraj
+        magnificationFactor
+    end
+    
+    methods
+        
+        function didSetRig(obj)
+            didSetRig@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+            [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
+        end
+         
+        function prepareRun(obj)
+            prepareRun@manookinlab.protocols.ManookinLabStageProtocol(obj);
+            obj.showFigure('symphonyui.builtin.figures.ResponseFigure', obj.rig.getDevice(obj.amp));
+            %get number of checkers...
+            canvasSize = obj.rig.getDevice('Stage').getCanvasSize();
+            %convert from microns to pixels...
+            stixelSizePix = obj.rig.getDevice('Stage').um2pix(obj.stixelSize);
+            obj.numChecksX = round(canvasSize(1) / stixelSizePix);
+            obj.numChecksY = round(canvasSize(2) / stixelSizePix);
+
+            % Get the resources directory.
+            obj.pkgDir = manookinlab.Package.getResourcePath();
+            
+            obj.currentStimSet = 'dovesFEMstims20160826.mat';
+            
+            % Load the current stimulus set.
+            obj.im = load([obj.pkgDir,'\',obj.currentStimSet]);
+            % Get the image and subject names.
+            if length(unique(obj.stimulusIndices)) == 1
+                obj.stimulusIndex = unique(obj.stimulusIndices);
+                obj.getImageSubject();
+            end
+         end
+        
+         function getImageSubject(obj)
+            % Get the image name.
+            obj.imageName = obj.im.FEMdata(obj.stimulusIndex).ImageName;
+            
+            % Load the image.
+            fileId = fopen([obj.pkgDir,'\doves\images\', obj.imageName],'rb','ieee-be');
+            img = fread(fileId, [1536 1024], 'uint16');
+            fclose(fileId);
+            
+            img = double(img');
+            img = (img./max(img(:))); %rescale s.t. brightest point is maximum monitor level
+            obj.backgroundIntensity = mean(img(:));%set the mean to the mean over the image
+            img = img.*255; %rescale s.t. brightest point is maximum monitor level
+            obj.imageMatrix = uint8(img);
+            
+            %get appropriate eye trajectories, at 200Hz
+            if (obj.freezeFEMs) %freeze FEMs, hang on fixations
+                obj.xTraj = obj.im.FEMdata(obj.stimulusIndex).frozenX;
+                obj.yTraj = obj.im.FEMdata(obj.stimulusIndex).frozenY;
+            else %full FEM trajectories during fixations
+                obj.xTraj = obj.im.FEMdata(obj.stimulusIndex).eyeX;
+                obj.yTraj = obj.im.FEMdata(obj.stimulusIndex).eyeY;
+            end
+            obj.timeTraj = (0:(length(obj.xTraj)-1)) ./ 200; %sec
+           
+            %need to make eye trajectories for PRESENTATION relative to the center of the image and
+            %flip them across the x axis: to shift scene right, move
+            %position left, same for y axis - but y axis definition is
+            %flipped for DOVES data (uses MATLAB image convention) and
+            %stage (uses positive Y UP/negative Y DOWN), so flips cancel in
+            %Y direction
+            obj.xTraj = -(obj.xTraj - 1536/2); %units=VH pixels
+            obj.yTraj = (obj.yTraj - 1024/2);
+            
+            %also scale them to canvas pixels. 1 VH pixel = 1 arcmin = 3.3
+            %um on monkey retina
+            %canvasPix = (VHpix) * (um/VHpix)/(um/canvasPix)
+            obj.xTraj = obj.xTraj .* 3.3/obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel');
+            obj.yTraj = obj.yTraj .* 3.3/obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel');
+            
+            % Load the fixations for the image.
+            f = load([obj.pkgDir,'\doves\fixations\', obj.imageName, '.mat']);
+            obj.subjectName = f.subj_names_list{obj.im.FEMdata(obj.stimulusIndex).SubjectIndex};
+            
+            % Get the magnification factor. Exps were done with each pixel
+            % = 1 arcmin == 1/60 degree; 200 um/degree...
+            if obj.manualMagnification > 1
+                obj.magnificationFactor = obj.manualMagnification;
+            else
+                obj.magnificationFactor = round(1/60*200/obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel'));
+            end
+        end
+        
+        function prepareEpoch(obj, epoch)
+            fprintf(1, 'start prepare epoc\n');
+            prepareEpoch@manookinlab.protocols.ManookinLabStageProtocol(obj, epoch);
+            
+            % Alternating seed for each epoch
+            if obj.useFixedSeed
+                obj.noiseSeed = 1;
+            else
+                obj.noiseSeed = randi(2^32 - 1);
+            end
+%             obj.noiseStream = RandStream('mt19937ar', 'Seed', obj.noiseSeed);
+            
+            % Toggle the seed usage for the next epoch
+            obj.useFixedSeed = ~obj.useFixedSeed;
+            
+            
+            %at start of epoch, set random stream
+%             obj.noiseStream = RandStream('mt19937ar', 'Seed', obj.noiseSeed);
+            epoch.addParameter('noiseSeed', obj.noiseSeed);
+            epoch.addParameter('numChecksX', obj.numChecksX);
+            epoch.addParameter('numChecksY', obj.numChecksY);
+            fprintf(1, 'end prepare epoc\n');
+         end
+
+         function p = createPresentation(obj)
+            fprintf(1, 'start create presentation\n');
+            p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3); %create presentation of specified duration
+            p.setBackgroundColor(obj.backgroundIntensity); % Set background intensity
+
+            %convert from microns to pixels...
+            apertureDiameterPix = obj.rig.getDevice('Stage').um2pix(obj.apertureDiameter);
+            
+            canvasSize = obj.rig.getDevice('Stage').getCanvasSize();
+
+            % Create checkerboard
+            initMatrix = uint8(255.*(obj.backgroundIntensity .* ones(obj.numChecksY,obj.numChecksX)));
+            board = stage.builtin.stimuli.Image(initMatrix);
+            board.size = canvasSize;
+            board.position = canvasSize/2;
+            board.setMinFunction(GL.NEAREST); %don't interpolate to scale up board
+            board.setMagFunction(GL.NEAREST);
+            p.addStimulus(board);
+
+            disp('pre lineMatcall')
+            obj.lineMatrix = util.getCheckerboardProjectLines(obj.noiseSeed, obj.numChecksX, obj.preTime, obj.stimTime, obj.tailTime, obj.backgroundIntensity,...
+                obj.frameDwell, obj.binaryNoise, obj.noiseStdv, 0, 1, obj.pairedBars);
+            disp('post line mat call')
+            
+            checkerboardController = stage.builtin.controllers.PropertyController(board, 'imageMatrix',...
+                @(state)getNewCheckerboard(obj, state.frame+1));
+            p.addController(checkerboardController); %add the controller
+            
+            if (obj.apertureDiameter > 0) %% Create aperture
+                aperture = stage.builtin.stimuli.Rectangle();
+                aperture.position = canvasSize/2;
+                aperture.color = obj.backgroundIntensity;
+                aperture.size = [max(canvasSize) max(canvasSize)];
+                mask = stage.core.Mask.createCircularAperture(apertureDiameterPix/max(canvasSize), 1024); %circular aperture
+                aperture.setMask(mask);
+                p.addStimulus(aperture); %add aperture
+            end
+            
+            % hide during pre & post
+            boardVisible = stage.builtin.controllers.PropertyController(board, 'visible', ...
+                @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
+            p.addController(boardVisible); 
+          
+            disp('post board visible')
+
+            function i = getNewCheckerboard(obj, frame)
+                line = obj.lineMatrix(:, frame);
+                i = uint8(255 * repmat(line', obj.numChecksY, 1));
+                size(i)
+            end
+            
+        end
+
+        function tf = shouldContinuePreparingEpochs(obj)
+            tf = obj.numEpochsPrepared < obj.numberOfAverages;
+        end
+        
+        function tf = shouldContinueRun(obj)
+            tf = obj.numEpochsCompleted < obj.numberOfAverages;
+        end
+        
+        function completeRun(obj)
+            completeRun@manookinlab.protocols.ManookinLabStageProtocol(obj);
+        end
+    end
+    
+end
